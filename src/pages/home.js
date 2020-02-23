15c81afaca907371d7424c104d02f94829da6d13
Lorenzo Rondan <dp1septemberlrd@gmail.com>
2020-02-23 18:44:13 +0100

    Start

---
+++ src/pages/home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+import Book from '../components/Book';
+
+ class home extends Component {
+    state = {
+        books: null
+    }
+    componentDidMount(){
+        axios.get('/books')
+        .then(res => {
+            console.log(res.data)
+            this.setState({
+                books: res.data
+            })
+        })
+        .catch(err => console.log(err));
+    }
+    render() {
+        let recentBooksMarkup = this.state.books ? (
+            this.state.books.map(book => <Book book={book}/>)
+        ) : <p>Loading...</p>
+        return (
+            <Grid container spacing={6}>
+                <Grid item sm={8} xs={12}>
+                    {recentBooksMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p> Profile </p>
+                </Grid>
+            </Grid>
+        )
+    }
+}
+
+export default home
