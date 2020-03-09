bb5ef2b8267e36305ee74b16272b8ee254bcc4b0
Lorenzo Rondan <dp1septemberlrd@gmail.com>
2020-03-10 00:00:19 +0100

    Arreglos de estilo

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import Book from '../components/Book';
 import Profile from '../components/Profile';
@@ -10,14 +11,18 @@ import Profile from '../components/Profile';
 import {connect} from 'react-redux';
 import {getBooks} from '../redux/actions/dataAction';
 
-
+const styles = {
+    progressBook: {
+        margin: '50px 50%'
+    }
+};
 class home extends Component {
     componentDidMount(){
         this.props.getBooks();
     };
     render() {
-        const {data: {books, loading}} = this.props;
-        let recentBooksMarkup = loading ? (<CircularProgress/>) : (
+        const {classes, data: {books, loading}} = this.props;
+        let recentBooksMarkup = loading ? (<CircularProgress className={classes.progressBook} />) : (
             books.map((book) => 
                 <Book key={book.bookId} book={book}/>)
         );
@@ -36,11 +41,11 @@ class home extends Component {
 
 home.propTypes = {
     getBooks: PropTypes.func.isRequired,
-    data: PropTypes.object.isRequired
+    data: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
     data: state.data
 });
 
-export default connect(mapStateToProps, {getBooks})(home);
+export default connect(mapStateToProps, {getBooks})(withStyles(styles)(home));
