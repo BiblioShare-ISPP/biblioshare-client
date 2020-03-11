a6e6bb3ce42962ecd2caf69233d6b00eb136ce9d
daniel <danielhingar3397@gmail.com>
2020-03-11 14:01:36 +0100

    Cambio store

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,16 +16,16 @@ const reducers = combineReducers({
     request: requestReducer
 });
 
-/*
+
 const store = createStore(reducers, initialState, compose(
     applyMiddleware(...middleware))
 );
-*/
 
+/*
 const store = createStore(reducers, initialState, compose(
     applyMiddleware(...middleware), 
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
-);
+);*/
 
 export default store;
