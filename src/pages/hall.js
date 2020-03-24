84d03562478d3ff25f4b496a24f620b03438e0e5
Loren <lrondan95@gmail.com>
2020-03-24 18:44:17 +0100

    Ayuntamiento v3

--- src/pages/hall.js
+++ src/pages/hall.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
@@ -30,23 +30,51 @@ const styles = theme => ({
 });
 
 class hall extends Component {
+    state = {
+        members: [],
+        accounts: 0,
+    };
+    componentWillReceiveProps(nextProps){
+        console.log('Entro')
+
+    }
     componentDidUpdate(prevProps){
         /* Funcion para obtener todos los habitantes de la region del ayuntamiento */
         if(this.props.hall.credentials !== prevProps.hall.credentials){
             this.props.getResidents(this.props.hall.credentials.location);
+            this.setState({
+                members: this.props.hall.credentials.members,
+                accounts: this.props.hall.credentials.accounts
+            });
         }
+    };
+    handleCheck = value => () => {
+        let newMember = this.state.members;
+        newMember.push(value);
+        let accounts = this.state.accounts - 1
+        this.setState({ 
+            members: newMember,
+            accounts: accounts
+         });
+        console.log(this.state);
+        /* Añadir a miembros del ayuntamiento el handle */
+        /* Dar tickets al usuario */
+        /* Quitar la cuenta al ayuntamiento */
+
     };
     render() {
         const {classes, hall:{loadingResidents, residents}} = this.props;
         let members = !loadingResidents ? (
-            residents.map((resident) => (
-                <ListItem button>
+            residents.map((resident, index) => (
+                <ListItem key={resident.handle} button>
                     <ListItemAvatar>
                         <Avatar alt={resident.handle} src={resident.imageUrl}/>
                     </ListItemAvatar>
-                    <ListItemText primary={resident.handle} />
+                    <ListItemText id={`checkbox-list-secondary-label-${index}`} primary={resident.handle} />
                     <ListItemSecondaryAction>
-                        <Checkbox edge="end"/>
+                        <Checkbox edge="end" 
+                            onChange={this.handleCheck(resident.handle)}
+                            inputProps={{ 'aria-labelledby': `checkbox-list-secondary-label-${index}` }}/>
                     </ListItemSecondaryAction>
                 </ListItem>
             ))): (<CircularProgress className={classes.progress} />);
