3a7e174a25537fca9a1f376c5ca63e88d59efae3
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2020-04-13 12:40:51 +0200

    Buscador miembro ayuntamiento

--- src/pages/hall.js
+++ src/pages/hall.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
+import {withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import HallProfile from '../components/HallProfile';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 import { withTranslation } from 'react-i18next';
 //Redux
-import {connect} from 'react-redux';
-import { getResidents, addUserToHall} from '../redux/actions/hallAction';
+import { connect } from 'react-redux';
+import { getResidents, addUserToHall } from '../redux/actions/hallAction';
 
 //MaterialUI
 import List from '@material-ui/core/List';
@@ -29,18 +31,53 @@ const styles = theme => ({
     },
     ad: {
         margin: '10px'
-    }
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: '#ffffff',
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+      },
+      searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      inputRoot: {
+        color: 'inherit',
+      },
+      inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200,
+          },
+        },
+      },
 });
 
 class hall extends Component {
     state = {
         members: [],
         accounts: 0,
-        location: ''
+        location: '',
+        search: ''
     };
-    componentDidUpdate(prevProps){
+    componentDidUpdate(prevProps) {
         /* Funcion para obtener todos los habitantes de la region del ayuntamiento */
-        if(this.props.hall.credentials !== prevProps.hall.credentials){
+        if (this.props.hall.credentials !== prevProps.hall.credentials) {
             this.props.getResidents(this.props.hall.credentials.location);
             this.setState({
                 members: this.props.hall.credentials.members,
@@ -53,43 +90,71 @@ class hall extends Component {
         let newMember = this.state.members;
         newMember.push(value);
         let accounts = this.state.accounts - 1
-        this.setState({ 
+        this.setState({
             members: newMember,
             accounts: accounts
-         });
+        });
 
         this.props.addUserToHall(value, this.state.location);
     };
+
+    updateSearch(event) {
+        this.setState({ search: event.target.value.substr(0, 60) });
+
+    }
+
     render() {
-        const {classes, hall:{loadingResidents, residents, credentials:{members, image, description, accounts}}} = this.props;
+        const { classes, hall: { loadingResidents, residents, credentials: { members, image, description, accounts } } } = this.props;
         const { t } = this.props;
+
+        let filteredResidents = residents.filter((member) => {
+            return member.handle.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        });
         let allResidents = !loadingResidents ? ((residents.length === 0) ? (
-                <ListItem button>
-                    <ListItemText>{t('noUsers')} {this.state.location}</ListItemText>
-                </ListItem>
-            ) : (
-                residents.map((resident, index) => (
+            <ListItem button>
+                <ListItemText>{t('noUsers')} {this.state.location}</ListItemText>
+            </ListItem>
+        ) : (
+
+                filteredResidents.map((resident, index) => (
+
                     <ListItem key={resident.handle} button>
                         <ListItemAvatar>
-                            <Avatar alt={resident.handle} src={resident.imageUrl}/>
+                            <Avatar alt={resident.handle} src={resident.imageUrl} />
                         </ListItemAvatar>
                         <ListItemText id={`checkbox-list-secondary-label-${index}`} primary={resident.handle} />
                         <ListItemSecondaryAction>
-                            <Checkbox edge="end" onChange={this.handleCheck(resident.handle)} checked={(members.includes(resident.handle)) ? true : false} disabled={((members.includes(resident.handle)) || (accounts<=0)) ? true : false} />
+                            <Checkbox edge="end" onChange={this.handleCheck(resident.handle)} checked={(members.includes(resident.handle)) ? true : false} disabled={((members.includes(resident.handle)) || (accounts <= 0)) ? true : false} />
                         </ListItemSecondaryAction>
                     </ListItem>
-            )))): (<CircularProgress className={classes.progress} />);
+                )))) : (<CircularProgress className={classes.progress} />);
         return (
             <Grid container spacing={6}>
                 <Grid item sm={4} xs={12}>
-                    <HallProfile/>
+                    <HallProfile />
                     <div className={classes.ad}>
                         <Typography variant="h5" color="primary">{description}</Typography>
-                        <img alt="Ad" src={image} width="100%"/>
+                        <img alt="Ad" src={image} width="100%" />
                     </div>
                 </Grid>
                 <Grid item sm={8} xs={12}>
                     <Typography variant="h4" color="primary">{t('Users')}</Typography>
+                    <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                                    <SearchIcon />
+                    </div>
+                    <InputBase
+                        name="search"
+                        onChange={this.updateSearch.bind(this)}
+                        placeholder={t('SearchByHandle')}
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                        value={this.state.search}
+                    />
+                    </div>
                     <br />
                     <List dense className={classes.root}>
                         {allResidents}
@@ -113,4 +178,4 @@ const mapStateToProps = state => ({
 });
 
 const hall1 = withTranslation()(hall);
-export default connect(mapStateToProps, {getResidents, addUserToHall})(withStyles(styles)(hall1));
+export default connect(mapStateToProps, { getResidents, addUserToHall })(withStyles(styles)(hall1));
