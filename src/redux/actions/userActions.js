145122e448448e815a0c08d69d8d016c525b7e84
Francisco <poli.ferreras@gmail.com>
2020-04-17 23:00:06 +0200

    Derecho a ser olvidado (GDPR)

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -116,7 +116,6 @@ export const editUserDetails = (userDetails) => (dispatch) => {
       .catch((err) => console.log(err));
   };
 
-
   export const updateTickets = (handle,tickets) => (dispatch) => {
     console.log('entro')
     axios.post(`/user/${handle}/${tickets}`)
@@ -125,3 +124,15 @@ export const editUserDetails = (userDetails) => (dispatch) => {
     })
     .catch(err => console.log(err));
 }
+
+export const deleteProfile = () => (dispatch) => {
+    dispatch({ type: LOADING_PROFILE });
+    axios.get(`/deleteUser`)
+    .then((res) => {
+        dispatch(logoutUser());
+        window.location.href = `/login`;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+};
